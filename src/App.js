ecb1496bcd5eb99f84e65997f792801a70975212
Kyoko Onoda <kobepikomaru@gmail.com>
2020-01-29 17:24:00 +0900

    Added the title, font, and background color

--- src/App.js
+++ src/App.js
@@ -32,10 +32,11 @@ class App extends Component {
       )
      return (
       <div className='App'>
-      <SearchBox
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
          placeholder='search monsters'
          handleChange={this.handleChange}
-      />
+         />
         <CardList monsters={ filteredMonsters } />
       </div>
     );
