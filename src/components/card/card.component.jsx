d5409e3a6002e666eb8faa6e7b92adfcc4c82e1a
lmalunin <lmalunin@gmail.com>
2022-10-09 17:49:45 +0300

    removed obsolete Card added key

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = ({monster}) => (
-    <div className='card-container' key={monster.id}>
+    <div className='card-container'>
         <img
             alt="monster"
             src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
